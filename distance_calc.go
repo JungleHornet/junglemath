8b1fbc1e15c6e5f1f3d21b1726d08dda9ecba921
JungleHornet <connore015@gmail.com>
2023-12-19 22:23:43 +0100

    added 3D distance calculation

--- distance_calc.go
+++ distance_calc.go
@@ -1,6 +1,7 @@
 package junglemath
 
 import (
+	"fmt"
 	"math"
 	"strconv"
 )
@@ -65,4 +66,54 @@ func CalcDistance(x1 float64, y1 float64, x2 float64, y2 float64, format format)
 	}
 
 	return "Error: format is not dec, rad, or simpRad"
+}
+
+func CalcDistance3D(x1, y1, z1, x2, y2, z2 float64, format format) string {
+	twoDimDist, err := strconv.ParseFloat(CalcDistance(x1, y1, x2, y2, "dec"), 64)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	twoDimDist = twoDimDist * twoDimDist
+	c := z2 - z1
+
+	c = c * c
+
+	dist := math.Sqrt(twoDimDist + c)
+
+	if format == Decimal {
+		return strconv.FormatFloat(dist, 'f', -1, 64)
+	}
+
+	sqrtDist := "√" + strconv.FormatFloat(math.Round(dist*dist), 'f', -1, 64)
+
+	if format == Radical {
+		return sqrtDist
+	}
+
+	root := math.Round(dist * dist)
+
+	rootCoefficient := int64(1)
+	simpleRootInt := root
+
+	for i := float64(2); i <= math.Round(math.Sqrt(root)); i++ {
+		if (simpleRootInt / i) == math.Trunc(simpleRootInt/i) {
+			if !(i == simpleRootInt) && !(i == 1) {
+				if math.Sqrt(simpleRootInt/i) == math.Trunc(math.Sqrt(simpleRootInt/i)) {
+					simpleRootInt = i
+					rootCoefficient = rootCoefficient * int64(math.Sqrt(root/i))
+				}
+			}
+		}
+	}
+
+	simpleRoot := strconv.FormatInt(rootCoefficient, 10) + "√" +
+		strconv.FormatFloat(simpleRootInt, 'f', -1, 64)
+
+	if format == SimplifiedRadical {
+		return simpleRoot
+	}
+
+	return "Error: format is not dec, rad, or simpRad"
+
 }
