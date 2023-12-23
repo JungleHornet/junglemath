248a98290658f16c2874c91d6b1e8d0232a29f8d
JungleHornet <connore015@gmail.com>
2023-12-23 21:13:38 +0100

    restructuring

--- calculator.go
+++ calculator.go
@@ -28,27 +28,19 @@ func Solve(equation string) float64 {
 	*/
 	regex1 := regexp.MustCompile("\\(.*\\)")
 	equation = strings.ReplaceAll(equation, " ", "")
-	inParentheses := regex1.FindString(equation)
-	solved := false
-	for !solved {
-		if inParentheses != "" {
-			inParentheses = strings.TrimLeft(inParentheses, "(")
-			inParentheses = strings.TrimRight(inParentheses, ")")
-			ans := Solve(inParentheses)
-			equation = strings.Replace(equation, "("+inParentheses+")", strconv.FormatFloat(ans, 'f', -1, 64), -1)
-		}
+	for inParentheses := regex1.FindString(equation); inParentheses != ""; inParentheses = regex1.FindString(equation) {
+		inParentheses = strings.TrimLeft(inParentheses, "(")
+		inParentheses = strings.TrimRight(inParentheses, ")")
+		ans := Solve(inParentheses)
+		equation = strings.Replace(equation, "("+inParentheses+")", strconv.FormatFloat(ans, 'f', -1, 64), -1)
+	}
 
-		regex2 := regexp.MustCompile("(-?\\d*.?\\d*)\\*(-?\\d*.?\\d*)")
-		for mult := regex2.FindStringSubmatch(equation); mult != nil; mult = regex2.FindStringSubmatch(equation) {
-			mult1, _ := strconv.ParseFloat(mult[1], 64)
-			mult2, _ := strconv.ParseFloat(mult[2], 64)
-			multRes := strconv.FormatFloat(mult1*mult2, 'f', -1, 64)
-			equation = strings.Replace(equation, mult[0], multRes, -1)
-		}
-		_, err := strconv.ParseFloat(equation, 64)
-		if err == nil {
-			solved = true
-		}
+	regex2 := regexp.MustCompile("(-?\\d*.?\\d*)\\*(-?\\d*.?\\d*)")
+	for mult := regex2.FindStringSubmatch(equation); mult != nil; mult = regex2.FindStringSubmatch(equation) {
+		mult1, _ := strconv.ParseFloat(mult[1], 64)
+		mult2, _ := strconv.ParseFloat(mult[2], 64)
+		multRes := strconv.FormatFloat(mult1*mult2, 'f', -1, 64)
+		equation = strings.Replace(equation, mult[0], multRes, -1)
 	}
 
 	ans, _ := strconv.ParseFloat(equation, 64)
