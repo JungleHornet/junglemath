bd58d07972546ec98c5ed8771c55a6268c410109
JungleHornet <connore015@gmail.com>
2024-02-25 13:16:20 -0600

    removed unnecessary Line.Fix() func

--- types.go
+++ types.go
@@ -4,18 +4,6 @@ type Point struct {
 	X, Y, Z float64
 }
 
-func (p *Point) Fix() {
-	if p.X == 0 {
-		p.X = 1
-	}
-	if p.Y == 0 {
-		p.Y = 1
-	}
-	if p.Z == 0 {
-		p.Z = 1
-	}
-}
-
 type Line struct {
 	P1, P2 Point
 }
